e52c6d2b571ab4435f6a229e02d729658dceb205
Jhosue Osuna <jhosue.osuna@gmail.com>
2026-07-15 13:56:18 -0500

    nuevos cambios

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  sassOptions: {
-    loadPaths: ["./src/styles"],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
